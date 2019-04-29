d16bbd5a16a3f29012553b296511479ebf371289
pdeppen <pdeppen17@aol.com>
2019-04-29 13:51:21 -0400

    #2: added Internet Router Class

--- src/routers/InternetRouter.java
+++ src/routers/InternetRouter.java
@@ -14,38 +14,33 @@ import java.util.Scanner;
 /**
  * Philip Deppen, Tyler Major
  */
-public abstract class AbstractRouter 
+public class InternetRouter 
 {
 	private Socket socket            = null;
     private DataOutputStream out     = null; 
     private InetAddress sendDest = null;
     private DataInputStream in       = null; 
-    private Scanner sc = null;
-    private byte[] failureMessage = {0,0,0,0,0,0,0,0,0,0};
-    protected int dest = 0;
-    protected int source = 0;
+    protected int lanDest = 0;
+    protected int lanSource = 0;
     protected byte[] message;
     protected byte checksum;
     protected boolean success;
-    protected int myClient;
-    protected int myClientPort;
     
     HashMap<Integer, String> routingMap = new HashMap<>();
     
-	public AbstractRouter() 
+	public InternetRouter() 
 	{
 //		this.listen();
 	}
 	
-	public abstract void initializeRouterMap();
 	
 	public void listen()
 	{
 		/* creates socket */
 		ServerSocket ssock = null;
 		try {
-			ssock = new ServerSocket(4446);
-			System.out.println("Router listening on port 4446...");
+			ssock = new ServerSocket(2555);
+			System.out.println("Internet Router listening on port 2555...");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -104,8 +99,9 @@ public abstract class AbstractRouter
 			e1.printStackTrace();
 		}
   		
-		this.source = message[0];
-		this.dest = message[1];
+		this.lanSource = message[3];
+		this.lanDest = message[4];
+		
 		System.out.println("Client Source: " + message[0]);
 		System.out.println("Client Dest: " + message[1]);
 		System.out.println("CheckSum: " + message[2]);
@@ -118,25 +114,16 @@ public abstract class AbstractRouter
 		
   		this.checksum = this.computeCheckSum();
   		
-//		String message = "From: " + this.source;
-		// TODO: remove this when using other clients
 		
 		// decide where to send message
-  		if (message[4] != 1)
-  		{
-  			if (this.success)
-  				this.sendToInternetRouter();
-  			else
-  				System.out.println("Message did not send: checksum failed");
-  		}
-  		else if (this.dest == this.myClient) 
+		if (this.lanDest == 1) 
 		{
-			this.sendToClient();
+			this.sendToRouterOne();
 		}
 		else 
 		{
 			if (this.success)
-				this.sendToRouter(dest, this.message);
+				this.sendToLan();
 			else 
 				System.out.println("Message did not send: checksum failed");
 		}
@@ -149,9 +136,9 @@ public abstract class AbstractRouter
 		}
 	}
 	
-	private void sendToInternetRouter() 
+	private void sendToLan() 
 	{
-		System.out.println("sending to: " + dest);
+		System.out.println("sending to: " + this.lanDest);
 		try {
 			sendDest = InetAddress.getByName("157.160.37.178");
 //			out.writeBoolean(false);
@@ -199,66 +186,13 @@ public abstract class AbstractRouter
         { 
             System.out.println(i); 
         } 
-	}
+	}		
 
-	public byte computeCheckSum()
+	private void sendToRouterOne() 
 	{
-		byte sum = 0;
-		byte numReqBytes = 0;
-		byte length = 10;
-		
-        byte[] newData = new byte[length];
-        for (int i = 0; i < length; i++)
-        {
-        		newData[i] = message[i];
-        }
-        
-        byte[] c1_data = new byte[length];
-    
-        
-//        System.out.println("C1_DATA");
-        for(int i = 5; i< newData.length; i++) 
-        {                  
-        		
-            // Complementing the data being received 
-            numReqBytes = (byte) ((Math.floor(Math.log(newData[i]) / Math.log(2))) + 1);  
-            c1_data[i] = (byte) (((1 << numReqBytes) - 1) ^ newData[i]); 
-            
-//            System.out.println(c1_data[i]);
-            // Adding the complemented data 
-            sum += c1_data[i]; 
-        } 
-//        System.out.println("Sum(in ones complement) is : "+sum);
-        
-     // Complementing the sum 
-        numReqBytes = (byte) ((Math.floor(Math.log(sum) / Math.log(2))) + 1);  
-        sum = (byte) (((1 << numReqBytes) - 1) ^ sum); 
-        System.out.println("Calculated Checksum is : "+sum); 
-          
-        // Checking whether final result is 0 or something else 
-        // and sending feedback accordingly  
-        if(sum == 0) 
-        {    
-            System.out.println("success"); 
-            this.success = true;
-        }      
-        else
-        {    
-        		this.success = false;
-            System.out.println("failure"); 
-        } 
-		
-		return sum;
-	}
-	
-	/**
-	 * Send the message that was received to the Client connected to the router (i.e. sends message to Client 1)
-	 */
-	public void sendToClient() 
-	{		              
-		System.out.println("sending to: " + dest);
+		System.out.println("sending to: " + this.lanDest);
 		try {
-			sendDest = InetAddress.getByName("157.160.37.183");
+			sendDest = InetAddress.getByName("157.160.37.180");
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -267,7 +201,7 @@ public abstract class AbstractRouter
 		// establish a connection 
         try
         { 
-            socket = new Socket(sendDest, this.myClientPort); 
+            socket = new Socket(sendDest, 4666); 
             System.out.println("Connected");  
   
             // sends output to the socket 
@@ -309,58 +243,68 @@ public abstract class AbstractRouter
             System.out.println(i); 
         } 
 	}
-	
-	public void sendToRouter(int dest, byte[] message) 
+
+
+	public byte computeCheckSum()
 	{
-		System.out.println("sending to: " + dest);
-		try {
-			sendDest = InetAddress.getByName(routingMap.get(dest));
-//			out.writeBoolean(false);
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
+		byte sum = 0;
+		byte numReqBytes = 0;
+		byte length = 10;
 		
-		System.out.println(routingMap.get(dest));
-		
-		// establish a connection 
-        try
-        { 
-            socket = new Socket(sendDest, 4446); 
-            System.out.println("Connected");  
-  
-            // sends output to the socket 
-            out    = new DataOutputStream(socket.getOutputStream()); 
-        } 
-        catch(UnknownHostException u) 
-        { 
-            System.out.println(u); 
-        } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
-        
-        // send user input to server
-        try
-        { 	
-    			out.write(message);
+        byte[] newData = new byte[length];
+        for (int i = 0; i < length; i++)
+        {
+        		newData[i] = message[i];
         }
         
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
+        byte[] c1_data = new byte[length];
+    
+        numReqBytes = (byte) ((Math.floor(Math.log(newData[2]) / Math.log(2))) + 1);  
+        c1_data[2] = (byte) (((1 << numReqBytes) - 1) ^ newData[2]); 
         
-        // close the connection 
-        try
-        { 
-            out.close(); 
-            socket.close(); 
+//        System.out.println(c1_data[i]);
+        // Adding the complemented data 
+        sum += c1_data[2]; 
+        
+//        System.out.println("C1_DATA");
+        for(int i = 5; i< newData.length; i++) 
+        {                  
+        		
+            // Complementing the data being received 
+            numReqBytes = (byte) ((Math.floor(Math.log(newData[i]) / Math.log(2))) + 1);  
+            c1_data[i] = (byte) (((1 << numReqBytes) - 1) ^ newData[i]); 
+            
+//            System.out.println(c1_data[i]);
+            // Adding the complemented data 
+            sum += c1_data[i]; 
         } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
+//        System.out.println("Sum(in ones complement) is : "+sum);
+        
+     // Complementing the sum 
+        numReqBytes = (byte) ((Math.floor(Math.log(sum) / Math.log(2))) + 1);  
+        sum = (byte) (((1 << numReqBytes) - 1) ^ sum); 
+        System.out.println("Calculated Checksum is : "+sum); 
+          
+        // Checking whether final result is 0 or something else 
+        // and sending feedback accordingly  
+        if(sum == 0) 
+        {    
+            System.out.println("success"); 
+            this.success = true;
+        }      
+        else
+        {    
+        		this.success = false;
+            System.out.println("failure"); 
         } 
+		
+		return sum;
 	}
+	
+	public static void main(String[] args)
+	{
+		InternetRouter ir = new InternetRouter();
+		ir.listen();
+	}
+			
 }
